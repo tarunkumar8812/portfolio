007c936551d0a2f0306da7c5ad25703823add3bf
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-02-16 13:15:20 +0530

    skills added

--- src/component/Aboutme.js
+++ src/component/Aboutme.js
@@ -10,6 +10,7 @@ import git from "../imgs/git.png"
 import html from "../imgs/html.jpg"
 import css from "../imgs/css.jpg"
 import express from "../imgs/express.png"
+import mysql from "../imgs/mysql.png"
 
 
 
@@ -22,7 +23,7 @@ const Aboutme = () => {
                     <h2>About Me</h2>
                     <p>
 
-                        Hello, I am Tarun Kumar, have a Civil Engineer Diploma from G.B. Pant Institute of Technology, Okhla Delhi in 2017. I have experience in teaching and running a coaching center, and have taught over 300+ students in the last 2 years. I am currently a freelancer, and Trainee at functionUp Bootcamp for backend development using Node JS, Express JS, MongoDB, and also AWS S3, Redis, HTML & CSS. I have completed various projects such as e-commerce website, book management system, blogging site, URL shortener, and college internship portal using these technologies.
+                        Hello, I am Tarun Kumar, have a Civil Engineer Diploma from G.B. Pant Institute of Technology, Okhla Delhi in 2017. I have experience in teaching and running a coaching center, and have taught over 200+ students in the last 2 years. I am currently a freelancer, and Trainee at functionUp Bootcamp for backend development using Node JS, Express JS, MongoDB, and also AWS S3, Redis, HTML & CSS. I have completed various projects such as e-commerce website, book management system, blogging site, URL shortener, and college internship portal using these technologies.
                     </p>
 
                     <p>
@@ -47,9 +48,8 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
-                            <img src={ postman} alt='postman'></img>
+                            <img src={postman} alt='postman'></img>
                             <p>POSTMAN</p>
                         </div>
                         <div className='m-container'>
@@ -59,7 +59,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={node} alt='nodejs'></img>
                             <p>NODEJS</p>
@@ -71,7 +70,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={redis} alt=''></img>
                             <p>REDIS</p>
@@ -83,7 +81,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={mongo} alt='mongo'></img>
                             <p>MONGODB</p>
@@ -95,7 +92,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={aws} alt='aws'></img><p
                             >AWS-S3</p>
@@ -107,7 +103,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={git} alt='git'></img>
                             <p>GIT</p>
@@ -119,7 +114,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={html} alt='html'></img>
                             <p>HTML5</p>
@@ -131,7 +125,6 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={css} alt='css'></img>
                             <p>CSS3</p>
@@ -143,13 +136,23 @@ const Aboutme = () => {
 
 
                     <div className='skill-name'>
-
                         <div className='img-container'>
                             <img src={express} alt='express'></img>
                             <p>EXPRESSJS</p>
                         </div>
                         <div className='m-container'>
                             <sapn className='m-9' ></sapn>
+                        </div>
+                    </div>
+
+
+                    <div className='skill-name'>
+                        <div className='img-container'>
+                            <img src={mysql} alt='mysql'></img>
+                            <p>MY-SQL</p>
+                        </div>
+                        <div className='m-container'>
+                            <sapn className='m-7' ></sapn>
                         </div>
                     </div>
 
