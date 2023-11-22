40c9823bd7c131c7fa9748d8e8035154e6e6171a
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-11-23 00:36:22 +0530

    full website is changed

--- src/projectData.js
+++ src/projectData.js
@@ -1,20 +1,58 @@
-
-
 import BookingAppImg from './assets/bookingApp.jpg'
 import noteApp from './assets/noteApp.jpg'
 import BookManagement from './assets/BookManagement.jpg'
+
 // import BookingAppImg from '../assets/'
 const projectData = [
     {
+        img: BookManagement,
+        link: "https://readersclub.vercel.app/",
+        gitHubLink: "https://github.com/tarunkumar8812/Online-Book-Store/tree/main/readersclub-client",
+        title: "Online Book Store",
+        desc: "Created an FullStack Book Management website's backend partwith multiple APIs based on products, users, books and auther. Here use JWT authentication for protected APIs and also use AWS. Like kindle.",
+        stack: "Tech Stack: JavaScript, NodeJS, ReactJs, MongoDB."
+    }, {
+        img: BookingAppImg,
+        link: "https://bookingclient.vercel.app/",
+        gitHubLink: "https://github.com/tarunkumar8812/bookingAppClient",
+        title: "Booking App",
+        desc: "Created an fullstack Hotel Booking website's and in backend partwith multiple APIs based on users, hotel, and room. Here use JWT authentication for protected APIs and also use AWS.",
+        stack: "Tech Stack: JavaScript, NodeJS, MongoDB, ReactJS."
+    }, {
+        img: noteApp,
+        link: "https://noteclient-khaki.vercel.app/",
+        gitHubLink: "",
+        title: "Noting App",
+        desc: "Created an fullstack Noting Application and in backend multiple CRUD Operations implemented like Create, Read, Update, Delete",
+        stack: "Tech Stack: JavaScript, NodeJS, MySQL, ReactJS."
+    }, {
+        img: "https://themewagon.com/wp-content/uploads/2020/12/eflyer.jpg",
+        link: "https://github.com/tarunkumar8812/Project_5-Products_Management/tree/Project-5",
+        gitHubLink: "",
+        title: "Product Mangement",
+        desc: "Created an eCommerce website's backend partwith multiple APIs based on products, users, carts and orders. Here use JWT authentication for protected APIs and also use AWS. Like Amazon and Flipkart etc.",
+        stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
+    }, {
+        img: "https://static.toiimg.com/photo/msid-74674451/74674451.jpg",
+        link: "https://github.com/tarunkumar8812/6",
+        gitHubLink: "",
+        title: "Banking System",
+        desc: "Created an Banking System website's backend partwith multiple APIs based on user, users, carts and orders. Here use JWT authentication for protected APIs and also use AWS. Like HDFC Bank.",
+        stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
+    },
+
+    {
         img: "https://images04.nicepage.com/feature/461183/website-blog.jpg",
         link: "https://github.com/tarunkumar8812/Project_1-Mini_Blogging_site",
+        gitHubLink: "",
         title: "Blogging Site",
         desc: "Created an Mini Blogging website's backend partwith multiple APIs based on users, author . Here use JWT authentication for protected APIs and also use AWS. Like blogging website.",
-        stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
+        stack: "Tech Stack: JavaScript, NodeJS, MongoDB.",
     },
     {
         img: "https://blog.hootsuite.com/wp-content/uploads/2020/08/Screen-Shot-2020-08-21-at-11.12.14-AM.png",
         link: "https://github.com/tarunkumar8812/Project_5-Products_Management/tree/Project-5",
+        gitHubLink: "",
         title: "Url Shortner",
         desc: "Created an URL Shortner website's backend partwith multiple APIs based on products, users, carts and orders. Here also use Redis Caching servise. Like Tiny URL.",
         stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
@@ -22,45 +60,15 @@ const projectData = [
     {
         img: "https://thumbs.dreamstime.com/b/internship-appretenceship-management-trainee-concept-people-79091934.jpg",
         link: "https://github.com/tarunkumar8812/Project_2-Open_To_Interns",
+        gitHubLink: "",
         title: "Open to Intern",
         desc: "Created an Open to Intern website's backend partwith multiple APIs based on college, student. Here use JWT authentication for protected APIs and also use AWS. Like Internshala etc.",
         stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
     },
-    {
-        img: "https://themewagon.com/wp-content/uploads/2020/12/eflyer.jpg",
-        link: "https://github.com/tarunkumar8812/Project_5-Products_Management/tree/Project-5",
-        title: "Product Mangement",
-        desc: "Created an eCommerce website's backend partwith multiple APIs based on products, users, carts and orders. Here use JWT authentication for protected APIs and also use AWS. Like Amazon and Flipkart etc.",
-        stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
-    },
 
-    {
-        img: "https://static.toiimg.com/photo/msid-74674451/74674451.jpg",
-        link: "https://github.com/tarunkumar8812/6",
-        title: "Banking System",
-        desc: "Created an Banking System website's backend partwith multiple APIs based on user, users, carts and orders. Here use JWT authentication for protected APIs and also use AWS. Like HDFC Bank.",
-        stack: "Tech Stack: JavaScript, NodeJS, MongoDB."
-    },
-    {
-        img: noteApp,
-        link: "https://noteclient-khaki.vercel.app/",
-        title: "Noting App",
-        desc: "Created an fullstack Noting Application and in backend multiple CRUD Operations implemented like Create, Read, Update, Delete",
-        stack: "Tech Stack: JavaScript, NodeJS, MySQL, ReactJS."
-    },
-    {
-        img: BookingAppImg,
-        link: "https://bookingclient.vercel.app/",
-        title: "Booking App",
-        desc: "Created an fullstack Hotel Booking website's and in backend partwith multiple APIs based on users, hotel, and room. Here use JWT authentication for protected APIs and also use AWS.",
-        stack: "Tech Stack: JavaScript, NodeJS, MongoDB, ReactJS."
-    }, {
-        img: BookManagement,
-        link: "https://readersclub.vercel.app/",
-        title: "Online Book Store",
-        desc: "Created an FullStack Book Management website's backend partwith multiple APIs based on products, users, books and auther. Here use JWT authentication for protected APIs and also use AWS. Like kindle.",
-        stack: "Tech Stack: JavaScript, NodeJS, ReactJs, MongoDB."
-    }
+
+
+
 ]
 
 
