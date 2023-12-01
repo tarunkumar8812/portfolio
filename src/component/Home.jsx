c87b29e7e2df00d02e3c3365dbc2edfd64883233
tarunkumar8812 <Tarunkumarkoli.tk@gmail.com>
2023-12-01 20:56:17 +0530

    profile pic is updated

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,11 +4,13 @@ import resume from '../assets/Resume_TarunKumar.pdf'
 
 const Home = () => {
     return (
-        <div className='home-container'>
+        <div className='home-container'
+            // style={{ backgroundImage: { profilePhoto }, backgroundPosition: "center", backgroundSize: "cover" }}
+        >
             <p>Full-Stack Web Developer</p>
             <h1>Hi, I'm </h1>
             <button className='btn'><a href={resume} download >Download CV</a></button>
-            {/* <img src='' alt='home-img'></img> */}
+            {/* <img src={profilePhoto} alt='home-img'></img> */}
         </div>
     )
 }
